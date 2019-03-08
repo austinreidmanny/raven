cc9c516b84893390c6809508a25dcea3687fdd46
Austin Manny <austin.manny@gmail.com>
2019-03-08 14:29:53 -0500

    Created driver script that will run a modularized DNAtax pipeline.

---
+++ dnatax_driver.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+# no SBATCH commands needed because this will just launch the modules
+
+# Set up a usage statement in case this program is called incorrectly
+usage() { echo "Make sure to provide a project name" \
+                "and one or more SRR accession numbers"
+					1>&2;
+					echo "Usage: $0 -p PROJECT -s SRR0001 (SRR0002) (SRR...)" \
+          1>&2; exit 1; }
+
+# Make sure the pipeline is invoked correctly, with project and sample names
+while getopts "p:s:" arg; do
+	case ${arg} in
+		p ) # Take in the project name
+		  PROJECT=${OPTARG}
+			;;
+		s ) # Take in the sample name(s)
+		  SAMPLES=${OPTARG}
+      ;;
+		* ) # Display help
+		  usage
+			;;
+	esac
+done
+
+shift $((OPTIND-1))
+
+# If the pipeline is not called correctly, tell that to the user and exit
+if [[ -z "${PROJECT}" ]] || [[ -z "${SAMPLES}" ]] ; then
+	usage
+fi
+
+# Print the project name and sample numbers to the screen
+echo "PROJECT name: ${PROJECT}"
+echo "SRA sample accession: ${SAMPLES}"
+
+# Make these available to all subsequent scripts
+export PROJECT
+export SAMPLES
+
+# Launch the setup script
+# sbatch -p short --mem 2GB -c 1 -t 00-00:05 ./setup.sh
+
+# Launch the script that downloads the SRA files from NCBI
+# ./download_sra.sh
+
+# Launch the adapter trimming script
+# ./adapter_trimming.sh
+
+# Launch the de novo assembly scripts
+# ./de_novo_assembly.sh
+
+# Launch the taxonomic classification script
+# ./classification.sh
+
+# Launch the script that converts NCBI taxonomy IDs to full taxonomic lineages
+# ./fetch_taxonomy.sh
+
+# Launch the script that extracts viral sequences from all the assembled contigs
+# ./extract_virus.sh
+
+# Launch the final save and cleanup script
+# ./cleanup.sh
